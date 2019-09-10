8ce1fc46ec7b805ab6c63fa8ce0199e304c19557
msizanoen <phanquangminh217@gmail.com>
2019-09-10 22:00:41 +0700

    Try to fix https://github.com/rust-lang/rust/issues/62558

--- src/main.rs
+++ src/main.rs
@@ -303,6 +303,13 @@ fn publish(pkg: &Package, commit: &str, vers: &semver::Version) {
                         }
                     }
                     new_table.insert("version".to_string(), toml::Value::String(vers.to_string()));
+                    if !has_package {
+                        new_table.insert(
+                            "package".to_string(),
+                            toml::Value::String(format!("{}-{}", PREFIX, name)),
+                        );
+                        has_package = true;
+                    }
                     let key_name = if has_package {
                         name.clone()
                     } else {
